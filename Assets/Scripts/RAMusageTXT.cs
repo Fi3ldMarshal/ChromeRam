a84e3964ac629d4a4a9ea0ebb723c9289a7b69f7
Fi3ldMarshal <Fi3ldMarshal@users.noreply.github.com>
2018-10-15 16:23:46 +0200

    Calculates RAM usage

---
+++ Assets/Scripts/RAMusageTXT.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RAMusageTXT : MonoBehaviour {
+
+    public int startSize = 128;
+
+    public int maxSize = 2048;
+    
+    Chrome player;
+
+    Text txt;
+
+    float playerStartSize = 1f;
+
+    float oldSize = 0f;
+
+    void Start () {
+        player = Chrome.main;
+        playerStartSize = player.size;
+        txt = GetComponent<Text>();
+        oldSize = playerStartSize;
+	}
+
+	void Update () {
+        if (oldSize != player.size)
+        {
+            txt.text = ((int)((player.size / playerStartSize) * startSize)).ToString() + "/" + maxSize;
+        }
+	}
+}
